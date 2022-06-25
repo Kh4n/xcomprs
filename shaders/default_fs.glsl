756c07dd708acc4b375476b3a8fb9bca1f9b5405
Kh4n <asadullahmkhan@gmail.com>
2022-06-25 00:57:53 -0500

    adjusted transform code at cost of my sanity

--- shaders/default_fs.glsl
+++ shaders/default_fs.glsl
@@ -8,7 +8,8 @@ uniform sampler2D win_texture;
 uniform sampler2D bg_texture;
 
 void main() {
-  vec2 tex_coord_win = vec2(tex_coord.x, 1-tex_coord.y);
-  frag_color =
-      texture(win_texture, tex_coord_win) + 0.1 * texture(bg_texture, tex_coord);
+  // xorg pixmap is inverted for some reason
+  vec2 tex_coord_win = vec2(tex_coord.x, 1 - tex_coord.y);
+  frag_color = texture(win_texture, tex_coord_win) +
+               0.1 * texture(bg_texture, tex_coord);
 }
